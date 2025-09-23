3dce57270ff1dac10f8f77e93f9f149de962acb2
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-09-23 21:18:09 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.DolgovIV.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DolgovIV.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(12, res);
+        }
+    }
+
+}
