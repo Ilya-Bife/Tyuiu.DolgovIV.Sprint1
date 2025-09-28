6d2e6a7156713d2001071e979fbd7d25b6f2e2f0
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-09-28 20:31:33 +0500

    Task3

---
+++ Tyuiu.DolgovIV.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.DolgovIV.Sprint1.Task3.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Долгов И. В. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: ператоры составного присваивания                                  *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #3                                                             *");
+        Console.WriteLine("* Выполнил: Долгов Илья Витальевич | АСОиУб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y, z;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите значение Z:");
+        z = Convert.ToInt32(Console.ReadLine());
+
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.ParallelepipedVolume(x,y,z));
+
+        Console.ReadLine();
+    }
+}
