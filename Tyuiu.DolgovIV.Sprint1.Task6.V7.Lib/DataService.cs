0f99825223379fac3135a2b891aac23ac8530591
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-09-29 16:41:08 +0500

    Task6

---
+++ Tyuiu.DolgovIV.Sprint1.Task6.V7.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.DolgovIV.Sprint1.Task6.V7.Lib
+{
+    public class DataService : ISprint1Task6V7
+    {
+        public string DeleteLastLetter(string value)
+        {
+            string res = "";
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (i == 0 || (value[i] == ' ' && i != value.Length && value[i-1]!= ' '))
+                {
+                    res += "";
+                }
+                else if (i != value.Length)
+                {
+                    res += value[i-1];
+                }
+
+                if (i == value.Length && value[i] == ' ')
+                {
+                    res += ' ';
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DolgovIV.Sprint1.Task6.V7.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.DolgovIV.Sprint1.Task6.V7.Lib;
+
+namespace Tyuiu.DolgovIV.Sprint1.Task6.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string name = " Hello Wo rld !";
+            Assert.AreEqual(ds.DeleteLastLetter(name), " Hell W rl ");
+        }
+    }
+}
